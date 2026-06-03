ac6ed6c10791c1a95931986437379d1e0c809104
fausc22 <faausc@gmail.com>
2026-06-03 16:09:14 -0300

    nueva url

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "res.cloudinary.com", pathname: "/**" },
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
       { protocol: "http", hostname: "**", pathname: "/**" },
       { protocol: "https", hostname: "**", pathname: "/**" },
